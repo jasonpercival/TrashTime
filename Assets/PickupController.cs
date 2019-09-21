cede84e7a5aac692573fa23676fe6ca6b4ef8d21
Jason <jaepercival@users.noreply.github.com>
2019-09-20 21:34:40 -0400

    Trash pickup trigger

---
+++ Assets/PickupController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour
+{
+
+    private Animator anim;
+    private bool isPickupReady = false;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+        if (isPickupReady && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Picked Up");
+            Destroy(gameObject);
+        }
+    }
+
+    // check if player enters pickup collider
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPickupReady = true;
+            anim.SetBool("IsReady", isPickupReady);
+        }
+    }
+
+    // check if player leaves pickup collider
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPickupReady = false;
+            anim.SetBool("IsReady", isPickupReady);
+        }
+    }
+}
